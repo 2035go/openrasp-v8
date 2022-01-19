5095b6c6a7ba816ba466296d875a46c1ae824e13
admin <admin@admin.com>
2022-01-19 21:18:55 +0800

    脚本换行符及一些错误

--- java/fetch_native_libraries.sh
+++ java/fetch_native_libraries.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo  "*****11 ****"
 
 set -ev
 ROOT=$(git rev-parse --show-toplevel)
@@ -9,7 +10,22 @@ if [ $TRAVIS ]; then
 else
   DIR=/tmp
 fi
+
+echo  "*****22 ****"
+
+
 echo $ROOT $TAG $DIR
-curl -R -# -k -L -o $DIR/java_natives_$TAG.tar.gz.download -z $DIR/java_natives_$TAG.tar.gz https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives.tar.gz
-[[ -f $DIR/java_natives_$TAG.tar.gz.download ]] && cp -p $DIR/java_natives_$TAG.tar.gz.download $DIR/java_natives_$TAG.tar.gz && rm $DIR/java_natives_$TAG.tar.gz.download
-tar zxf $DIR/java_natives_$TAG.tar.gz -C $ROOT/java/src/main/resources
+#/home/wang/code/openrasp/openrasp-v8 v21 /tmp
+
+echo  "*****33 ****"
+echo https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives.tar.gz
+# https://github.com/baidu-security/openrasp-v8/releases/download/v21/java_natives.tar.gz
+
+#curl -R -# -k -L -o $DIR/java_natives_$TAG.tar.gz.download -z $DIR/java_natives_$TAG.tar.gz https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives.tar.gz
+
+echo  "*****44 ****"
+
+
+#[[ -f $DIR/java_natives_$TAG.tar.gz.download ]] && cp -p $DIR/java_natives_$TAG.tar.gz.download $DIR/java_natives_$TAG.tar.gz && rm $DIR/java_natives_$TAG.tar.gz.download
+#tar zxf $DIR/java_natives_$TAG.tar.gz -C $ROOT/java/src/main/resources
+tar zxf java_natives.tar.gz -C $ROOT/java/src/main/resources
